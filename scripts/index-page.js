d693642952f5f25220df393be938b95f97e1f383
kchu187 <kchung187@gmail.com>
2023-12-10 16:37:08 -0700

    add preliminary functionality for comment section

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,21 +1,26 @@
-const commentForm = document.getElementById("comments__form");
-const commentSection = document.querySelector(".comments");
-const comments = [];
+//Define Variables for Comment section
 
-window.onload = commentForm;
-console.log(commentForm);
-console.log(comments);
+const commentForm = document.querySelector(".comments__form");
+const commentSection = document.querySelector(".comments__container");
 
+const comments = []; // Create an empty array to store comments
+
+// // window.onload = commentForm;
+// console.log(commentForm);
+// console.log(comments);
+
+//Create function for pushing new comments into comments array
 commentForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const commentName = event.target.nameInput.value;
   const commentContent = event.target.commentInput.value;
 
-  if (nameInput !== "" && commentInput !== "") {
+  if (commentName !== "" && commentContent !== "") {
     comments.push({
-      name: nameInput,
-      comment: commentInput,
+      name: commentName,
+      content: commentContent,
     });
+
     renderComments();
     event.target.reset();
   } else {
@@ -23,4 +28,25 @@ commentForm.addEventListener("submit", (event) => {
   }
 });
 
+console.log(comments);
+
+function renderComments() {
+  commentSection.innerHTML = "";
+  comments.forEach(function (comment) {
+    const commentDiv = document.createElement("div");
+    const commentNameTitle = document.createElement("h2");
+    commentNameTitle.innerText = comment.name;
+
+    const commentContent = document.createElement("p");
+    commentContent.innerText = comment.content;
+
+    commentDiv.appendChild(commentNameTitle);
+    commentDiv.appendChild(commentContent);
+    commentSection.appendChild(commentDiv);
+  });
+}
+
+console.log[comments[1]];
+
+// renderComments();
 console.log(commentForm);
