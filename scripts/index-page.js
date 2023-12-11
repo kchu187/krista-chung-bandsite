405d304fd8910f3dfedfa51a7a39ad2cacdf70ba
kchu187 <kchung187@gmail.com>
2023-12-11 00:45:09 -0700

    finish functionality for comments + styling individual comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,7 +1,7 @@
 //Grab from DOM + define Variables for Comment section
 
 const commentForm = document.querySelector(".comments__form");
-const commentSection = document.querySelector(".comments__container");
+const commentSection = document.querySelector(".comments__list");
 
 // Create an empty array to store comments
 const comments = [];
@@ -11,48 +11,79 @@ const User1Comment = {
   name: "Miles Acosta",
   content:
     "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  timestamp: "12/20/2020",
 };
 
 const User2Comment = {
   name: "Emilie Beach",
   content:
     "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day",
+  timestamp: "12/20/2020",
 };
 
 const User3Comment = {
   name: "Connor Walton",
   content:
     "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  timestamp: "12/20/2020",
 };
 
 //Push the existing/default comments into the Object array
-comments.push(User3Comment, User2Comment, User1Comment);
+comments.push(User1Comment, User2Comment, User3Comment);
 
 //Create a loop to renderdefault comments onto page
-for (let i = 0; i < comments.length; i++) {
-  //For each object in the array, create a new Div to contain name+content and classes.
-  const commentInitDiv = document.createElement("div");
-  commentInitDiv.classList.add("comments__item");
+function renderInitComment() {
+  for (let i = 0; i < comments.length; i++) {
+    //For each object in the array, create a new Div to contain name+content and classes.
+    //Note c will be used as abbreviation for "comment", an to make variables less visually cluttered
 
-  //Create a new paragraph element with a class for name values
-  const commentInitNameTitle = document.createElement("p");
-  commentInitNameTitle.classList.add("comments__name");
-  commentInitNameTitle.innerText = comments[i].name;
+    const commentDefList = document.createElement("li");
+    commentDefList.classList.add("comments__item");
 
-  //Create a new paragraph element with a class for comment contents
-  const commentInitContent = document.createElement("p");
-  commentInitContent.classList.add("comments__content");
-  commentInitContent.innerText = comments[i].content;
+    //Create an avatar placeholder
+    const commentAvatar = document.createElement("div");
+    commentAvatar.classList.add("comments__avatar");
 
-  //Append the div onto comment section and the Name + content as child of div
-  commentInitDiv.appendChild(commentInitNameTitle);
-  commentInitDiv.appendChild(commentInitContent);
-  commentSection.appendChild(commentInitDiv);
+    //Create a new paragraph element with a class for name values
+    const commentDefName = document.createElement("p");
+    commentDefName.classList.add("comments__name");
+    commentDefName.innerText = comments[i].name;
+
+    //Create a new paragraph element with a class for comment contents
+    const commentDefContent = document.createElement("p");
+    commentDefContent.classList.add("comments__content");
+    commentDefContent.innerText = comments[i].content;
+
+    //Create new paragraph element with a class for timestamp
+    const commentTimestamp = document.createElement("p");
+    commentTimestamp.classList.add("comments__timestamp");
+    commentTimestamp.innerText = comments[i].timestamp;
+
+    //Create a container/subsection to separate avatar and comment contents
+    const commentSubsection = document.createElement("div");
+    commentSubsection.classList.add("comments__subsection");
+
+    //Create a container for name and timestamp
+    const commentNameContainer = document.createElement("div");
+    commentNameContainer.classList.add("comments__name-container");
+
+    //Append the div onto comment section and the Name + content as child of div
+    commentDefList.appendChild(commentAvatar);
+    commentDefList.appendChild(commentSubsection);
+    commentSubsection.appendChild(commentNameContainer);
+    commentNameContainer.appendChild(commentDefName);
+    commentNameContainer.appendChild(commentTimestamp);
+    commentSubsection.appendChild(commentDefContent);
+    commentSection.prepend(commentDefList);
+  }
 }
+
+renderInitComment();
 
 //Add EventListener to add input values of HTML form into comments array
 commentForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  comments.splice(0, comments.length);
   const commentName = event.target.nameInput.value;
   const commentContent = event.target.commentInput.value;
 
@@ -60,28 +91,37 @@ commentForm.addEventListener("submit", (event) => {
     comments.unshift({
       name: commentName,
       content: commentContent,
+      timestamp: Date.now(),
     });
 
-    renderComments();
+    renderInitComment();
     event.target.reset();
   } else {
     alert("Please enter some text");
   }
 });
 
-//Create a function to render new comments from the HTML form
-function renderComments() {
-  commentSection.innerHTML = "";
-  comments.forEach((comment) => {
-    const commentDiv = document.createElement("div");
-    const commentNameTitle = document.createElement("p");
-    commentNameTitle.innerText = comment.name;
+//Create a function to render new comments from the HTML form ---Can possibly deprecate)
+// function renderComments() {
+//   commentSection.innerHTML = "";
+//   comments.forEach((comment) => {
+//     const commentList = document.createElement("li");
+//     commentList.classList.add("comments__item");
+//     const commentNameTitle = document.createElement("p");
+//     commentNameTitle.innerText = comment.name;
 
-    const commentContent = document.createElement("p");
-    commentContent.innerText = comment.content;
+//     const commentContent = document.createElement("p");
+//     commentContent.innerText = comment.content;
 
-    commentDiv.appendChild(commentNameTitle);
-    commentDiv.appendChild(commentContent);
-    commentSection.appendChild(commentDiv);
-  });
-}
+//     commentList.appendChild(commentNameTitle);
+//     commentList.appendChild(commentContent);
+//     commentSection.appendChild(commentList);
+//   });
+// }
+
+// const currentDate = new Date();
+// const timestamp = currentDate.getTime();
+
+// console.log(timestamp);
+
+// console.log(Date.now());
