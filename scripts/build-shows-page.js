e211e8b3e807bf7d6abd92e2b6a7dd50bb13036e
Krista Chung <kchung187@gmail.com>
2023-12-14 05:14:55 -0700

    Add soundcloud iFrame + create JS array for shows

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,11 @@
+// Create an array containing all shows data
+
+const shows = [
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+];
+
+console.log(shows[0]);
