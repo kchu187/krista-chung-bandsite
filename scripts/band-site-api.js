28df72c07c390b7a9cc60c4b6b5ce3900790d9e4
kchu187 <kchung187@gmail.com>
2024-01-14 01:40:22 -0700

    adjust bandsite api format + create getShows method

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,152 +1,94 @@
-const url = "https://project-1-api.herokuapp.com/";
+class BandSiteAPI {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseURL = "https://project-1-api.herokuapp.com/";
+  }
 
-const apiKey = "763ae118-ef81-46d0-b66d-44fb83b2cf2e";
-let commentsData = [];
-//Retrieve the 3 existing default comments from the API and display them
-
-const getComments = async () => {
-  try {
-    const commentsResp = await axios.get(
-      "https://project-1-api.herokuapp.com/comments?api_key=${apiKey}"
+  //Method for fetching shows data via API and displaying it
+  getShows = async () => {
+    const showsResp = await axios.get(
+      this.baseURL + "showdates?api_key=" + this.apiKey
     );
 
-    console.log(commentsResp.data);
-    //Store the Comments data from the API into a variable
-    commentsData = commentsResp.data;
+    console.log(showsResp.data[1]);
 
-    //Sort each comment by timestamp, earliest comment on the bottom
-    commentsData.sort((date1, date2) => {
-      return date1.timestamp - date2.timestamp;
-    });
+    const showsData = showsResp.data;
+    console.log(Object.keys(showsData[0]));
+    console.log(showsData[0]);
+    const showsHeaders = Object.keys(showsData[0]);
 
-    const commentSection = document.querySelector(".comments__list");
-    console.log(commentsData[1]);
+    showsData.forEach((show) => {
+      //Create an article section for each show
+      const showsArticle = document.createElement("article");
+      showsArticle.classList.add("shows__block");
+      showsContainer.appendChild(showsArticle);
 
-    // Clear the comment section first to avoid double-rendering
-    commentSection.innerHTML = "";
-    // For each object in the array, create a new div containing name+comment elements
-    commentsData.forEach((comment) => {
-      const commentDefList = document.createElement("li");
-      commentDefList.classList.add("comments__item");
-
-      //Create an avatar placeholder
-      const commentAvatar = document.createElement("div");
-      commentAvatar.classList.add("comments__avatar");
-
-      //Create a new paragraph element with a class for name values
-      const commentDefName = document.createElement("p");
-      commentDefName.classList.add("comments__name");
-      commentDefName.innerText = comment.name;
-
-      const commentDefContent = document.createElement("p");
-      commentDefContent.classList.add("comments__content");
-      commentDefContent.innerText = comment.comment;
+      //Create headers for date
+      const dateHeader = document.createElement("h2");
+      dateHeader.classList.add("shows__header--mobile");
+      dateHeader.innerText = "DATE";
+      showsArticle.appendChild(dateHeader);
 
       //Create read-able timestamps
-      const timestamp = comment.timestamp;
+      const timestamp = show.date;
 
       const localTimeStamp = (timestamp) => {
         const localTime = new Date(timestamp);
-        const date = localTime.getDate();
-        const month = localTime.getMonth() + 1;
-        const year = localTime.getFullYear();
-        return month + "/" + date + "/" + year;
+        const options = {
+          weekday: "short",
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        };
+
+        return localTime.toLocaleDateString("en-US", options);
       };
 
       const formattedTime = localTimeStamp(timestamp);
+      //Create paragraph for date data
+      const dateData = document.createElement("p");
+      dateData.classList.add("shows__data--date");
+      dateData.innerText = formattedTime;
+      showsArticle.appendChild(dateData);
 
-      //Create new paragraph element with a class for timestamp
-      const commentTimestamp = document.createElement("p");
-      commentTimestamp.classList.add("comments__timestamp");
-      commentTimestamp.innerText = formattedTime;
-      //Create a container/subsection to separate avatar and comment contents
-      const commentSubsection = document.createElement("div");
-      commentSubsection.classList.add("comments__subsection");
+      // Create heading for venue
 
-      //Create a container for name and timestamp
-      const commentNameContainer = document.createElement("div");
-      commentNameContainer.classList.add("comments__name-container");
+      const venueHeader = document.createElement("h2");
+      venueHeader.classList.add("shows__header--mobile");
+      venueHeader.innerText = "VENUE";
+      showsArticle.appendChild(venueHeader);
 
-      //Append the div onto comment section and the Name + content as child of div
-      commentDefList.appendChild(commentAvatar);
-      commentDefList.appendChild(commentSubsection);
-      commentSubsection.appendChild(commentNameContainer);
-      commentNameContainer.appendChild(commentDefName);
-      commentNameContainer.appendChild(commentTimestamp);
-      commentSubsection.appendChild(commentDefContent);
-      commentSection.prepend(commentDefList);
+      //Create paragraph for venue data
+      const venueData = document.createElement("p");
+      venueData.classList.add("shows__data");
+      venueData.innerText = show.place;
+      showsArticle.appendChild(venueData);
+
+      //Create heading for location
+      const locationHeader = document.createElement("h2");
+      locationHeader.classList.add("shows__header--mobile");
+      locationHeader.innerText = "LOCATION";
+      showsArticle.appendChild(locationHeader);
+
+      //Create paragraph for location data
+      const locationData = document.createElement("p");
+      locationData.classList.add("shows__data");
+      locationData.innerText = show.location;
+      showsArticle.appendChild(locationData);
+
+      // Create a button to buy tickets
+      const showsButton = document.createElement("a");
+      showsButton.classList.add("shows__button");
+      showsButton.innerText = "BUY TICKETS";
+      showsArticle.appendChild(showsButton);
     });
-  } catch (error) {
-    console.error(error);
-  }
-};
-getComments();
 
-//Add EventListener to HTMl form to push into comments array via api
-const commentForm = document.querySelector(".comments__form");
-
-commentForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  //Create variables for form elements
-  const commentName = event.target.nameInput.value;
-  const commentContent = event.target.commentInput.value;
-  const commentNameField = document.querySelector(
-    ".comments__form-field--name"
-  );
-  const commentContentField = document.querySelector(
-    ".comments__form-field--comment"
-  );
-
-  const postComment = async () => {
-    await axios.post(
-      "https://project-1-api.herokuapp.com/comments?api_key=${apiKey}",
-      {
-        name: commentName,
-        comment: commentContent,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    getComments();
+    try {
+    } catch (error) {
+      console.log(error);
+    }
   };
+}
 
-  if (commentName !== "" && commentContent !== "") {
-    postComment();
-    commentNameField.style.removeProperty("border");
-    commentContentField.style.removeProperty("border");
-    //Reset the form once submitted
-    event.target.reset();
-    // getComments();
-    // Add additional validation checks for each blank field scenario
-  } else if (commentName == "" && commentContent == "") {
-    alert("Please enter some text");
-    commentNameField.style.border = "solid 0.063rem red";
-    commentContentField.style.border = "solid 0.063rem red";
-  } else if (commentName == "") {
-    commentNameField.style.border = "solid 0.063rem red";
-    alert("Please enter a name!");
-  } else if (commentContent == "") {
-    commentContentField.style.border = "solid 0.063rem red";
-    alert("Please write a comment!");
-  }
-});
-
-//getShows function to render show information via API
-
-const getShows = async () => {
-  const showsResp = await axios.get(
-    "https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}"
-  );
-
-  console.log(showsResp.data);
-  try {
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getShows();
+//Create instance for BandSiteAPI class
+const myBandSiteApi = new BandSiteAPI("763ae118-ef81-46d0-b66d-44fb83b2cf2e");
--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,125 +1,9 @@
-// Create an array containing all shows data
-
-const showsData = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-  },
-];
-
-console.log(showsData[1]);
-console.log(Object.keys(showsData[0]));
-
-const showsHeaders = Object.keys(showsData[0]);
-
-// window.matchMedia("(max-width:768px");
-
-// Create a container for all Shows
 const showsContainer = document.createElement("div");
 showsContainer.classList.add("shows__container");
 
 //Append this container to Shows section
 const showsSection = document.querySelector(".shows");
 showsSection.appendChild(showsContainer);
-//Create a media query for screen sizes above mobile
-
-// Create a function to render shows data into a table
-function renderShows() {
-  showsData.forEach((show) => {
-    //Create an article section for each show
-    const showsArticle = document.createElement("article");
-    showsArticle.classList.add("shows__block");
-    showsContainer.appendChild(showsArticle);
-
-    //Create headers for date
-    const dateHeader = document.createElement("h2");
-    dateHeader.classList.add("shows__header--mobile");
-    dateHeader.innerText = "DATE";
-    showsArticle.appendChild(dateHeader);
-
-    //Create paragraph for date data
-    const dateData = document.createElement("p");
-    dateData.classList.add("shows__data--date");
-    dateData.innerText = show.date;
-    showsArticle.appendChild(dateData);
-
-    // Create heading for venue
-
-    const venueHeader = document.createElement("h2");
-    venueHeader.classList.add("shows__header--mobile");
-    venueHeader.innerText = "VENUE";
-    showsArticle.appendChild(venueHeader);
-
-    //Create paragraph for venue data
-    const venueData = document.createElement("p");
-    venueData.classList.add("shows__data");
-    venueData.innerText = show.venue;
-    showsArticle.appendChild(venueData);
-
-    //Create heading for location
-    const locationHeader = document.createElement("h2");
-    locationHeader.classList.add("shows__header--mobile");
-    locationHeader.innerText = "LOCATION";
-    showsArticle.appendChild(locationHeader);
-
-    //Create paragraph for location data
-    const locationData = document.createElement("p");
-    locationData.classList.add("shows__data");
-    locationData.innerText = show.location;
-    showsArticle.appendChild(locationData);
-
-    // Create a button to buy tickets
-    const showsButton = document.createElement("a");
-    showsButton.classList.add("shows__button");
-    showsButton.innerText = "BUY TICKETS";
-    showsArticle.appendChild(showsButton);
-  });
-}
-renderShows();
-
-// Create media query for mobile sizing
-// function mobileHeaderDisplay() {
-//   const mobileHeaders = document.querySelectorAll(".shows__header--mobile");
-//   const mobileSizing = window.matchMedia("(min-width: 768px)");
-
-//   mobileHeaders.forEach((header) => {
-//     if (mobileSizing.matches) {
-//       header.style.display = "none";
-//     } else {
-//       header.style.display = "block";
-//     }
-//   });
-// }
-// mobileHeaderDisplay();
-//
-//Add Event Listener everytime window is re-sized to check the screen size again by calling
-window.addEventListener("resize", mobileHeaderDisplay);
 
 //Create function when clicking on show to apply selected state
 
@@ -137,3 +21,5 @@ document.addEventListener("click", function (event) {
     clickedBlock.classList.add("active");
   }
 });
+// const myBandSiteApi = new BandSiteAPI("763ae118-ef81-46d0-b66d-44fb83b2cf2e");
+myBandSiteApi.getShows();
